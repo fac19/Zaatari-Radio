aa38e6c737a6ff9d4c0c9b425fdead159f8ba52f
James-Hardy <jamesjhardy95@gmail.com>
2020-06-08 17:43:01 +0100

    ZTRI-19 #in-progress #time 1h 40m #comment Add routes, fix linter and require proptype validation
    Co-authored-by: vatskan <vatskan@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import AllWorkshops from './components/AllWorkshops';
+import WorkshopOverview from './components/WorkshopOverview';
+import WorkshopFeedback from './components/WorkshopFeedback';
+import WorkshopWorksheet from './components/WorkshopWorksheet';
+import WorkshopContent from './components/WorkshopContent';
+
 import './App.css';
 
 function App() {
-	return <div>Hi</div>;
+	return (
+		<>
+			<Router>
+				<div className="App">
+					{/* <NavBar currentPage={currentPage} setCurrentPage={setCurrentPage} /> */}
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/workshops" component={AllWorkshops} />
+						<Route exact path="/workshop/overview/:ID">
+							<WorkshopOverview />
+						</Route>
+						<Route exact path="/workshop/feedback/:ID" component={WorkshopFeedback} />
+						<Route exact path="/workshop/content/:ID" component={WorkshopContent} />
+						<Route exact path="/workshop/worksheet/:ID" component={WorkshopWorksheet} />
+					</Switch>
+				</div>
+			</Router>
+		</>
+	);
 }
 
 export default App;
