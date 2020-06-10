225525d8cb55a3b0f60664855ac7d2b5db47b9fa
Ivo Evans <ievans.storrie@gmail.com>
2020-06-10 10:55:35 +0100

    ZTRI-30 #time 10m #comment Make BackButton a styled component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Title1, BGYellow } from '../styles/componentsStyles';
+import BackButton from './buttons/back';
 
 export default function Home() {
 	return (
 		<>
 			<BGYellow>
+				<BackButton to="/workshops" />
 				<Title1>Home</Title1>
 			</BGYellow>
 		</>
--- src/components/buttons/back.js
+++ src/components/buttons/back.js
@@ -2,20 +2,23 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const BlackButton = styled(Button)`
+	background-color: #07031a;
+	color: #f2f2f2;
+`;
 
 export default function BackButton({ to }) {
 	const history = useHistory();
 	return (
-		<Button
-			variant="contained"
-			color="primary"
-			href="#contained-buttons"
+		<BlackButton
 			onClick={() => {
 				history.push(to);
 			}}
 		>
-			Home
-		</Button>
+			Back
+		</BlackButton>
 	);
 }
 
