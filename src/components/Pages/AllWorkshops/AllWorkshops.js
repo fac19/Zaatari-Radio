3924482541746925f18f3134747d1ebcfd8b5eb1
Joe Jackson <joe.m.jackson@live.co.uk>
2020-06-11 17:14:11 +0100

    ZTRI-13 #in-progress #time 1h #comment Style the card like LA Lakers
    Co-authored-by: vatskan  <vatskan@gmail.com>

--- src/components/Pages/AllWorkshops/AllWorkshops.js
+++ src/components/Pages/AllWorkshops/AllWorkshops.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { Title1, BGYellow } from '../../../styles/componentsStyles';
-// import api from '../../../api/api';
+import api from '../../../api/api';
 
 const StyledMain = styled.section`
 	display: flex;
@@ -16,15 +16,21 @@ const StyledArticle = styled.article`
 	width: 80vw;
 	max-width: 40rem;
 	background: whitesmoke;
+	border-radius: 1%;
+	box-shadow: 5px 5px 5px 5px hsla(289, 32%, 47%, 0.72);
 `;
 
 const StyledImage = styled.img`
 	width: 100%;
+	border-radius: 1% 1% 0 0;
 `;
 
 const StyledCardTitle = styled.h2`
 	font-family: DINCondensed;
 	font-size: 30px;
+	font-weight: bold;
+	color: black;
+	text-transform: uppercase;
 	margin: 0;
 	padding: 8px 0 5px 0;
 `;
@@ -35,52 +41,68 @@ const StyledButton = styled.a`
 
 const StyledButtonContainer = styled.div`
 	background: #ffcb74;
-	margin: 0 40% 0 0.5rem;
+	margin: 0 20% 0 0.5rem;
+	padding-left: 0.5rem;
+`;
+
+const StyledWorkshopDetails = styled.div`
+	display: flex;
+	justify-content: space-between;
+	margin: 0.5rem 20% 0.5rem 0.5rem;
+	padding: 0.5rem;
+	border: solid 1px lightgrey;
+`;
+
+const StyledSpan = styled.span`
+	width: 33%;
+	text-align: left;
+	font-family: roboto;
 `;
 
 export default function AllWorkshops() {
-	const allWorkshopData = true; // delete after styling complete.
-
-	// const [allWorkshopData, setAllWorkshopData] = React.useState(true);
-	// const [error, setError] = React.useState('');
-	// React.useEffect(() => {
-	// 	api
-	// 		.getWorkshops()
-	// 		.then((result) => {
-	// 			// console.log(result)
-	// 			// console.log("title", result.records[0].fields.title)
-	// 			setAllWorkshopData(result.records);
-	// 		})
-	// 		.catch((err) => {
-	// 			setError(err.details);
-	// 		});
-	// }, []);
+	const [allWorkshopData, setAllWorkshopData] = React.useState(null);
+	const [error, setError] = React.useState('');
+	React.useEffect(() => {
+		api
+			.getWorkshops()
+			.then((result) => {
+				setAllWorkshopData(result.records);
+			})
+			.catch((err) => {
+				setError(err.details);
+			});
+	}, []);
 
 	function createWorkshopList() {
+		const data = allWorkshopData[0];
+		// const workshopCardData = {
+		// 	imageUrl: data.fields.carousel_pictures[0].url,
+		// 	title: data.fields.title,
+		// 	time: data.fields.table_of_contents,
+		// 	comments: data.fields.feedback.length,
+		// 	rating: data.fields.average_rating
+		// }
+
 		// console.log('createWorkshopList -> allWorkshopData', allWorkshopData);
-		// const data = allWorkshopData[0];
+		// console.log("createWorkshopList -> workshopCardData", workshopCardData)
+
 		return (
 			<StyledArticle>
-				{/* <img src={data.fields.carousel_pictures[0].url} />
-				<ul>
-					<li>{data.fields.title}</li>
-					<li>{data.fields.table_of_contents}</li>
-					<li>{data.fields.average_rating}</li>
-					<li>{data.fields.feedback.length}</li>
-					<li>{data.id}</li>
-				</ul> */}
-				<StyledImage src="https://dl.airtable.com/.attachments/75768831999f01ca14c3f6930ce8bbb6/2be86799/WS1.PNG" />
-				<ul>
-					<StyledButtonContainer>
-						<StyledButton as={Link} to="/">
-							<StyledCardTitle>Workshop uno</StyledCardTitle>
-						</StyledButton>
-					</StyledButtonContainer>
-					<li>3</li>
-					<li>4</li>
-					<li>6</li>
-					<li>reclXZv3JtH77KpzW</li>
-				</ul>
+				<StyledImage src={data.fields.carousel_pictures[0].url} />
+
+				<StyledButtonContainer>
+					<StyledButton as={Link} to="/">
+						<StyledCardTitle>{data.fields.title}</StyledCardTitle>
+					</StyledButton>
+				</StyledButtonContainer>
+				<StyledWorkshopDetails>
+					<StyledSpan>Time: {data.fields.table_of_contents}</StyledSpan>
+					<StyledSpan>Comments: {data.fields.feedback.length}</StyledSpan>
+					<StyledSpan>Rating: {data.fields.average_rating}</StyledSpan>
+				</StyledWorkshopDetails>
+				{/* <div>
+						<li>reclXZv3JtH77KpzW</li>
+					</div> */}
 			</StyledArticle>
 		);
 	}
@@ -88,7 +110,7 @@ export default function AllWorkshops() {
 	return (
 		<>
 			<BGYellow>
-				{/* {error || ''} */}
+				{error || ''}
 				<Title1>Workshops</Title1>
 			</BGYellow>
 			<StyledMain>{allWorkshopData ? createWorkshopList() : <h1>Loading...</h1>}</StyledMain>
