5130ecbf4b807af25549fda59fb33cc2c2969176
Vatsal Kanoria <vatskan@gmail.com>
2020-06-11 13:07:22 +0100

    ZTRI-13 #in-progress #time 40m #comment display title for one workshop
    Co-authored-by: joepock123 <joe.m.jackson@live.co.uk>

--- src/components/Pages/AllWorkshops/AllWorkshops.js
+++ src/components/Pages/AllWorkshops/AllWorkshops.js
@@ -3,24 +3,35 @@ import { Title1, BGYellow } from '../../../styles/componentsStyles';
 import api from '../../../api/api';
 
 export default function AllWorkshops() {
+	const [allWorkshopData, setAllWorkshopData] = React.useState(null);
 	const [error, setError] = React.useState('');
 	React.useEffect(() => {
 		api
 			.getWorkshops()
 			.then((result) => {
-				return result;
+				// console.log(result)
+				// console.log("title", result.records[0].fields.title)
+				setAllWorkshopData(result.records);
 			})
 			.catch((err) => {
 				setError(err.details);
 			});
 	}, []);
 
+	function createWorkshopList() {
+		// console.log("createWorkshopList -> allWorkshopData", allWorkshopData)
+		const data = allWorkshopData[0];
+		return <h1>{data.fields.title}</h1>;
+	}
+
 	return (
 		<>
 			<BGYellow>
 				{error || ''}
 				<Title1>Workshops</Title1>
+				{/* {allWorkshopData ? createWorkshopList() : (<h1>Loading...</h1>) } */}
 			</BGYellow>
+			<BGYellow>{allWorkshopData ? createWorkshopList() : <h1>Loading...</h1>}</BGYellow>
 		</>
 	);
 }
