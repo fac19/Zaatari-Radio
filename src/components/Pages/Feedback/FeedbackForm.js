d7ce939621ae1f98899dc307375582d5361a0eb1
Vatsal Kanoria <vatsal@MacBook-Pro.lan>
2020-06-16 17:08:18 +0100

    ZTRI-18 #in-progress #time 10m #comment small changes to input names

--- src/components/Pages/Feedback/FeedbackForm.js
+++ src/components/Pages/Feedback/FeedbackForm.js
@@ -6,31 +6,45 @@ const StyledForm = styled.form`
 	display: flex;
 	flex-direction: column;
 	width: 70vw;
+	margin-top: 8rem;
 `;
 
 export default function FeedbackForm() {
 	const { register, handleSubmit, errors } = useForm();
-	const onSubmit = (data) => console.log('data', data);
-	console.log(errors);
+	const onSubmit = (data) => {
+		console.log('data', data);
+		/* data looks like: 
+      {
+        attendees: "2"
+        comments: "asdf"
+        email: "asdf@asdf.com"
+        name: "asdf asdf"
+        organisation: "asdf"
+        starRating: " 5"
+        studentFeedback: "asdf"
+      }
+    */
+	};
+	console.log('error', errors);
 
 	return (
 		<>
 			<StyledForm onSubmit={handleSubmit(onSubmit)}>
 				<label htmlFor="name">
-					Name:
-					<input id="name" type="text" placeholder="" name="Name" ref={register({ required: true, pattern: /.+\s.+/i })} />
+					Full Name:
+					<input id="name" type="text" placeholder="" name="name" ref={register({ required: true, pattern: /.+\s.+/i })} />
 				</label>
 				<label htmlFor="email">
 					Email:
-					<input id="email" type="text" placeholder="" name="Email" ref={register({ required: true, pattern: /\S+@\S+\.\S+/i })} />
+					<input id="email" type="text" placeholder="" name="email" ref={register({ required: true, pattern: /\S+@\S+\.\S+/i })} />
 				</label>
 				<label htmlFor="organisation">
 					Organisation:
-					<input id="organisation" type="text" placeholder="" name="Organisation" ref={register} />
+					<input id="organisation" type="text" placeholder="" name="organisation" ref={register} />
 				</label>
 				<label htmlFor="attendees">
 					How many attendees were there?
-					<input id="attendees" type="number" placeholder="0" name="How many attendees were there?" ref={register({ required: true })} />
+					<input id="attendees" type="number" placeholder="0" name="attendees" ref={register({ required: true })} />
 				</label>
 				<fieldset>
 					<legend>Rating:</legend>
