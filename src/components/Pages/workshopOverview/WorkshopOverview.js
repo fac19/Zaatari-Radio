2e68b84c48d58f7c04f87c8db117419780ed810f
Joe Jackson <joe.m.jackson@live.co.uk>
2020-06-17 23:52:41 +0100

    ZTRI-87 #in-progress #time 20m #comment Fix error handling for WorkshopOverview. Remove janky line breaks and add ternary

--- src/components/Pages/workshopOverview/WorkshopOverview.js
+++ src/components/Pages/workshopOverview/WorkshopOverview.js
@@ -11,7 +11,7 @@ import Comments from './Comments';
 import BackButton from '../../buttons/BackButton';
 
 export default function WorkshopOverview({ match: { params } }) {
-	const [errorState, setErrorState] = React.useState('');
+	const [errorState, setErrorState] = React.useState(null);
 	const [workshop, setWorkshop] = React.useState({});
 	useSpecificWorkshop(params.ID, setWorkshop, setErrorState);
 
@@ -19,12 +19,16 @@ export default function WorkshopOverview({ match: { params } }) {
 		<>
 			<WorkshopHeader images={workshop.images} date={workshop.date_created} tags={workshop.tags} title={workshop.title} />
 			<SC.MainContainer>
-				<Intro authorArr={workshop.workshop_authors} equipment={null} />
-				<Main overview={workshop.overview} id={params.ID} />
-				<Comments feedbackArr={workshop.feedback} />
+				{errorState ? (
+					<SC.Title>{errorState}</SC.Title>
+				) : (
+					<>
+						<Intro authorArr={workshop.workshop_authors} equipment={null} />
+						<Main overview={workshop.overview} id={params.ID} />
+						<Comments feedbackArr={workshop.feedback} />
+					</>
+				)}
 				<BackButton to="/workshops" />
-
-				{errorState}
 			</SC.MainContainer>
 		</>
 	);
