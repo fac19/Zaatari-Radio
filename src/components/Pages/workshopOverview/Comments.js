aab458511934b24d4bb71c19af42efc6cccda6e1
Joe Jackson <joe.m.jackson@live.co.uk>
2020-06-17 23:08:06 +0100

    ZTRI-87 #in-progress #time 20m #comment Refactor styles for Header component

--- src/components/Pages/workshopOverview/Comments.js
+++ src/components/Pages/workshopOverview/Comments.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../../api/api';
 
-import CommentListItem from './CommentsList';
+import CommentListItem from './CommentsListItem';
 import * as SC from './style';
 
 export default function Comments({ feedbackArr }) {
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -16,3 +16,6 @@ export const cardWidth = '40rem';
 // font-families
 export const headerFont = 'DIN, sans-serif';
 export const standardFont = 'Roboto, sans-serif';
+
+// Font sizes
+export const DinHeaderBig = '45px';
