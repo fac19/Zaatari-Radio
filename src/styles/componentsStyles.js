77fe7f9aca6e03e24eee73a0549be575da36c47d
Ivo Evans <ievans.storrie@gmail.com>
2020-06-18 18:43:14 +0100

    ZTRI-89 #in-progress #time 10m #comment Delete redundant styled components

--- src/styles/componentsStyles.js
+++ src/styles/componentsStyles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import * as vars from './variables';
 
+// eslint-disable-next-line import/prefer-default-export
 export const Title1 = styled.h1`
 	text-transform: uppercase;
 	font-family: ${vars.headerFont};
@@ -10,27 +10,4 @@ export const Title1 = styled.h1`
 	font-size: 45px;
 	line-height: 45px;
 	color: #322f2f;
-`;
-
-export const NavTitle = styled.h1`
-	margin: 0 2rem;
-	text-transform: uppercase;
-	font-family: ${vars.headerFont};
-	font-style: normal;
-	font-weight: normal;
-	font-size: 30px;
-	line-height: 50px;
-	color: ${vars.offWhite};
-`;
-
-export const StyledLink = styled(Link)`
-	color: inherit;
-	text-decoration: inherit;
-	&:focus,
-	&:hover,
-	&:visited,
-	&:link,
-	&:active {
-		text-decoration: none;
-	}
 `;
