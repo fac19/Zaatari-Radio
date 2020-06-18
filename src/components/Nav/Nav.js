76308bf247482b535a1fee8f8db1e3c03192672f
VatsKan <vatskan@gmail.com>
2020-06-18 16:51:38 +0100

    ZTRI-107 #done #time 30m #comment nav bar looking good
    Co-authored-by: joepock123 <joe.m.jackson@live.com>

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+const NavLink = styled(Link)`
+	color: inherit;
+	text-decoration: none;
+
+	&:hover {
+		color: inherit;
+		text-decoration: none;
+	}
+`;
 
 export default function NavBar() {
 	return (
 		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-			<Navbar.Brand href="#home">HumaniBase</Navbar.Brand>
+			<Navbar.Brand>
+				<NavLink to="/">HumaniBase</NavLink>
+			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 			<Navbar.Collapse id="responsive-navbar-nav">
 				<Nav className="mr-auto">
-					<Nav.Link href="/">Home</Nav.Link>
-					<Nav.Link href="/workshops">Find Workshops</Nav.Link>
-					<Nav.Link href="/contact">Contact</Nav.Link>
+					<Nav.Link>
+						<NavLink to="/">Home</NavLink>
+					</Nav.Link>
+					<Nav.Link>
+						<NavLink to="/workshops">Find Workshops</NavLink>
+					</Nav.Link>
+					<Nav.Link>
+						<NavLink to="/contact">Contact</NavLink>
+					</Nav.Link>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
