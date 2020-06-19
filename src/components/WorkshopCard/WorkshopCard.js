883ce06506f3f1974b1d2dd603fa1d45af6c8d0e
Vatsal Kanoria <vatskan@gmail.com>
2020-06-19 12:33:13 +0100

    fix app break bug if data undefined

--- src/components/WorkshopCard/WorkshopCard.js
+++ src/components/WorkshopCard/WorkshopCard.js
@@ -4,20 +4,34 @@ import PropTypes from 'prop-types';
 import * as SC from './style';
 
 export default function WorkshopCard({ data }) {
+	// this dataObj ensures that if any of the fields in the airtable is empty, it doesn't break the app
+	// can refactor this to handle the error like in the workshop overview page
+	// (e.g. using the error handling in the getFromJSON function)
+	// the WorkshopCard component should also be moved into a children folder of AllWorkshops
+	// the allWorkshops needs to be refactored to use hooks like the other pages have
+	const dataObj = {
+		id: data.id ? data.id : '',
+		image: data.fields.carousel_pictures ? data.fields.carousel_pictures[0].url : '',
+		title: data.fields.title ? data.fields.title : '',
+		duration: data.fields.duration ? data.fields.duration : '',
+		feedbackLength: data.fields.feedback ? data.fields.feedback.length : '',
+		averageRating: data.fields.average_rating ? data.fields.average_rating : '',
+	};
+
 	return (
 		<SC.Article>
-			<Link to={`/workshop/overview/${data.id}`}>
-				<SC.Image src={data.fields.carousel_pictures[0].url} />
+			<Link to={`/workshop/overview/${dataObj.id}`}>
+				<SC.Image src={dataObj.image} />
 			</Link>
 			<SC.ButtonContainer>
-				<SC.Button as={Link} to={`/workshop/overview/${data.id}`}>
-					<SC.CardTitle>{data.fields.title}</SC.CardTitle>
+				<SC.Button as={Link} to={`/workshop/overview/${dataObj.id}`}>
+					<SC.CardTitle>{dataObj.title}</SC.CardTitle>
 				</SC.Button>
 			</SC.ButtonContainer>
 			<SC.WorkshopDetails>
-				<SC.Span>Duration: {data.fields.duration}hrs</SC.Span>
-				<SC.Span>Feedback: {data.fields.feedback.length}</SC.Span>
-				<SC.Span>Rating: {data.fields.average_rating}</SC.Span>
+				<SC.Span>Duration: {dataObj.duration}hrs</SC.Span>
+				<SC.Span>Feedback: {dataObj.feedbackLength}</SC.Span>
+				<SC.Span>Rating: {dataObj.averageRating}</SC.Span>
 			</SC.WorkshopDetails>
 		</SC.Article>
 	);
