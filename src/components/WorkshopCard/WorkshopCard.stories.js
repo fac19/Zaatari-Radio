e5fe1d1d588186ac339810400034543ec994867d
Joe Jackson <joe.m.jackson@live.co.uk>
2020-06-19 12:11:19 +0100

    ZTRI-132 #in-progress #time 40m #comment Add edge cases for workshop card stories and define default props

--- src/components/WorkshopCard/WorkshopCard.stories.js
+++ src/components/WorkshopCard/WorkshopCard.stories.js
@@ -7,22 +7,53 @@ export default {
 	title: 'WorkshopCard',
 };
 
-const data = {
-	id: 'reclXZv3JtH77KpzW',
-	fields: {
-		carousel_pictures: [
-			{
-				url: 'https://dl.airtable.com/.attachments/75768831999f01ca14c3f6930ce8bbb6/2be86799/WS1.PNG',
-			},
-		],
-		duration: 4,
-		feedback: ['recvoGLUVrUrhDQ4o', 'recADAnl5iZEOu9bI'],
-		average_rating: 4,
-		title: 'Workshop Number 1',
-	},
+export const StandardCard = () => {
+	const data = {
+		id: 'reclXZv3JtH77KpzW',
+		fields: {
+			carousel_pictures: [
+				{
+					url: 'https://dl.airtable.com/.attachments/75768831999f01ca14c3f6930ce8bbb6/2be86799/WS1.PNG',
+				},
+			],
+			duration: '4',
+			feedback: ['recvoGLUVrUrhDQ4o', 'recADAnl5iZEOu9bI'],
+			average_rating: 4,
+			title: 'Storybook Workshop',
+		},
+	};
+
+	return (
+		<SC.Main>
+			<WorkshopCard data={data} />
+		</SC.Main>
+	);
 };
 
-export const StandardCard = () => {
+export const noPropsCard = () => {
+	return (
+		<SC.Main>
+			<WorkshopCard />
+		</SC.Main>
+	);
+};
+
+export const noIdCard = () => {
+	const data = {
+		id: undefined,
+		fields: {
+			carousel_pictures: [
+				{
+					url: 'https://dl.airtable.com/.attachments/75768831999f01ca14c3f6930ce8bbb6/2be86799/WS1.PNG',
+				},
+			],
+			duration: '4',
+			feedback: ['recvoGLUVrUrhDQ4o', 'recADAnl5iZEOu9bI'],
+			average_rating: 4,
+			title: 'Storybook Workshop',
+		},
+	};
+
 	return (
 		<SC.Main>
 			<WorkshopCard data={data} />
@@ -31,6 +62,44 @@ export const StandardCard = () => {
 };
 
 export const OversizedImageCard = () => {
+	const data = {
+		id: undefined,
+		fields: {
+			carousel_pictures: [
+				{
+					url: 'https://picsum.photos/id/237/1400/600',
+				},
+			],
+			duration: '4',
+			feedback: ['recvoGLUVrUrhDQ4o', 'recADAnl5iZEOu9bI'],
+			average_rating: 4,
+			title: 'Storybook Workshop',
+		},
+	};
+
+	return (
+		<SC.Main>
+			<WorkshopCard data={data} />
+		</SC.Main>
+	);
+};
+
+export const UndefinedImageCard = () => {
+	const data = {
+		id: undefined,
+		fields: {
+			carousel_pictures: [
+				{
+					url: undefined,
+				},
+			],
+			duration: '4',
+			feedback: ['recvoGLUVrUrhDQ4o', 'recADAnl5iZEOu9bI'],
+			average_rating: 4,
+			title: 'Storybook Workshop',
+		},
+	};
+
 	return (
 		<SC.Main>
 			<WorkshopCard data={data} />
